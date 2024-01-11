88b93449546848a9c81ed3d1e843910579fec2cd
tiffany_k <tiffany.kantz@gmail.com>
2024-01-11 18:49:44 -0500

    Created a calculation function

--- js/script.js
+++ js/script.js
@@ -14,3 +14,18 @@ subDurationElement.addEventListener("change", function (e) {
     subDuration = Number(e.target.value);
     console.log(subDuration);
 });
+
+var updateSubscriptionDiv = function () {
+    var monthlyCost = 5;
+
+    if (subType === "standard") {
+        monthlyCost = 7
+    } else if (subType === "premium") {
+        monthlyCost = 10;
+    }
+
+    var total = subDuration * monthlyCost;
+
+    result.innerText = `You have chosen a ${subDuration} month ${subType} plan for $${total}.`
+
+};
